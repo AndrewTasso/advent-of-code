7bb2cc61799a6e147e2079b5933986efc0ae4b03
Andrew A. Tasso <andrew@tasso.dev>
2021-12-02 16:23:48 -0500

    Add solution for Day 2 Part 2

--- src/main/kotlin/dev/tasso/adventofcode2021/day02/Day02.kt
+++ src/main/kotlin/dev/tasso/adventofcode2021/day02/Day02.kt
@@ -9,8 +9,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 150)
-    //TODO solve this
-    check(part2(testInput) == 6)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
     println("Part 1: ${part1(input)}")
@@ -26,7 +25,7 @@ fun part1(input: List<String>): Int {
     // We can only go forward, so strip the direction text, and sum all the values to get the total
     val horizontalTotal = horizontalMovements.map { it.removePrefix("forward ") }
                                              .sumOf { it.toInt() }
-    // Up is positive, down is negative. Strip the direction text if we're going up, or replace with a  negative sign.
+    // Up is positive, down is negative. Strip the direction text if we're going up, or replace with a negative sign.
     // Then, sum all the values to get the total
     val verticalTotal = verticalMovements.map { it.removePrefix("up ") }
                                          .map { it.replace("down ", "-") }
@@ -35,4 +34,28 @@ fun part1(input: List<String>): Int {
     return horizontalTotal * verticalTotal.absoluteValue
 }
 
-fun part2(input: List<String>): Int = input.size
+fun part2(input: List<String>): Int {
+
+    var horizontalPosition = 0
+    var verticalPosition = 0
+    var aim = 0
+
+    for(command in input) {
+
+        if(command.startsWith("forward ")) {
+
+            val forwardDistance = command.removePrefix("forward ").toInt()
+            horizontalPosition += forwardDistance
+            verticalPosition += forwardDistance * aim
+
+        } else if (command.startsWith("up ") || command.startsWith("down ")){
+
+            val verticalDistance = command.removePrefix("up ").replace("down ", "-").toInt()
+            aim += verticalDistance
+
+        }
+
+    }
+
+    return horizontalPosition * verticalPosition.absoluteValue
+}
