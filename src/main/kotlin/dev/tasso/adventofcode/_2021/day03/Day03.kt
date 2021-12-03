03bcb4404192fd3576338db8426c9058eea340ed
Andrew A. Tasso <andrew@tasso.dev>
2021-12-03 03:37:28 -0500

    Solve Day 03 Part 2

--- src/main/kotlin/dev/tasso/adventofcode/_2021/day03/Day03.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2021/day03/Day03.kt
@@ -7,7 +7,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
-    check(part2(testInput) == 12)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println("Part 1: ${part1(input)}")
@@ -30,15 +30,51 @@ fun part1(input: List<String>): Int {
     val gamma = Integer.parseInt(gammaString, 2)
     val epsilon = Integer.parseInt(epsilonString, 2)
 
-    println("$gammaString $gamma")
-    println("$epsilonString $epsilon")
-
     return  gamma * epsilon
 
 }
 
 fun part2(input: List<String>): Int {
 
-    return input.size
+    var oxygenGeneratorValues = input.toList()
+    var co2ScrubberValues = input.toList()
+
+    for(i in 0 until oxygenGeneratorValues[0].length) {
+
+        oxygenGeneratorValues = if (oxygenGeneratorValues.count { s -> s[i] == '1' } >= oxygenGeneratorValues.count { s -> s[i] == '0' }) {
+
+            oxygenGeneratorValues.filter { s -> s[i] == '1' }
+
+        } else {
+
+            oxygenGeneratorValues.filter { s -> s[i] == '0' }
+
+        }
+
+        if (oxygenGeneratorValues.size == 1) break
+
+    }
+
+    for(i in 0 until co2ScrubberValues[0].length) {
+
+        co2ScrubberValues = if (co2ScrubberValues.count { s -> s[i] == '0' } <= co2ScrubberValues.count { s -> s[i] == '1' }) {
+
+            co2ScrubberValues.filter { s -> s[i] == '0' }
+
+        } else {
+
+            co2ScrubberValues.filter { s -> s[i] == '1' }
+
+        }
+
+        if (co2ScrubberValues.size == 1) break
+
+    }
+
+    val oxygenGeneratorRating = Integer.parseInt(oxygenGeneratorValues[0], 2)
+    val co2ScrubberRating = Integer.parseInt(co2ScrubberValues[0], 2)
+
+    return oxygenGeneratorRating * co2ScrubberRating
 }
+
 
