1374c2deb742a6a0ea7a95cbc21862ca33711258
Andrew A. Tasso <andrew@tasso.dev>
2021-12-04 23:55:42 -0500

    Solve Day 4 part 2

--- src/main/kotlin/dev/tasso/adventofcode/_2021/day04/Day04.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2021/day04/Day04.kt
@@ -7,11 +7,11 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 4512)
-    check(part2(testInput) == 19)
+    println(part2(testInput) == 1924)
 
     val input = readInput("Day04")
     println("Part 1: ${part1(input)}")
-//    println("Part 2: ${part2(input)}")
+    println("Part 2: ${part2(input)}")
 
 }
 
@@ -48,7 +48,32 @@ fun part1(input: List<String>): Int {
 
 fun part2(input: List<String>): Int {
 
-    return input.size
+    val calledNumbers = input[0].split(",")
+
+    val cards = input.asSequence()
+                     .drop(2)
+                     .filterNot { it == "" }
+                     .map{it.trim().split(Regex("""\s+"""))}
+                     .chunked(5)
+                     .map{ BingoCard(it)}
+                     .toList()
+
+    var lastWinningCard: BingoCard? = null
+
+    for(number in calledNumbers) {
+        cards.filter { !it.hasBingo() }.forEach { it.numberCalled(number) }
+
+        val nonWinningCards = cards.filter { !it.hasBingo() }
+
+        if(nonWinningCards.size == 1) {
+            lastWinningCard = nonWinningCards[0]
+        }
+        if(nonWinningCards.isEmpty()) {
+            break
+        }
+    }
+
+    return lastWinningCard!!.getUnmarkedNumbers().map{ it.toInt() }.sum() * lastWinningCard.lastCalledNumber.toInt()
 
 }
 
