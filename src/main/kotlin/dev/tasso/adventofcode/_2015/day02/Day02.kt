886d0619b8d012e78ad1640728ba464adfb508be
Andrew A. Tasso <andrew@tasso.dev>
2021-12-09 23:57:37 -0500

    Solve AOC 2015 Day 2 part 2

--- src/main/kotlin/dev/tasso/adventofcode/_2015/day02/Day02.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2015/day02/Day02.kt
@@ -15,7 +15,15 @@ class Day02 : Solution<Int> {
     }
 
     override fun part2(input: List<String>): Int {
-        TODO("Not yet implemented")
+
+        val dimensions = input.map { dimension -> dimension.split("x").map { it.toInt() } }
+
+        return dimensions.sumOf{ dimension ->
+
+            getWrapLength(dimension[0], dimension[1], dimension[2]) + getVolume(dimension[0], dimension[1], dimension[2])
+
+        }
+
     }
 }
 
@@ -24,3 +32,9 @@ fun getSurfaceArea(length : Int, width : Int, height : Int) =
 
 fun getSlack(length : Int, width : Int, height : Int) =
     listOf(length * width, width * height, height * length).minOrNull()!!
+
+fun getWrapLength(length : Int, width : Int, height : Int) =
+    listOf(length, width, height).sorted().take(2).sumOf { shortSide -> shortSide * 2 }
+
+fun getVolume(length : Int, width : Int, height : Int) = length * width * height
+
--- src/test/kotlin/dev/tasso/adventofcode/_2015/day02/Day02Test.kt
+++ src/test/kotlin/dev/tasso/adventofcode/_2015/day02/Day02Test.kt
@@ -8,5 +8,7 @@ class Day02Test : BehaviorSpec({
 
     include(solutionTest(Day02()::part1, 58, "2015/Day02_test_pt1_01.txt"))
     include(solutionTest(Day02()::part1, 43, "2015/Day02_test_pt1_02.txt"))
+    include(solutionTest(Day02()::part2, 34, "2015/Day02_test_pt1_01.txt"))
+    include(solutionTest(Day02()::part2, 14, "2015/Day02_test_pt1_02.txt"))
 
 })
