b4976b8a976fbd45307c3e1475a9e694659824c3
Andrew A. Tasso <andrew@tasso.dev>
2021-12-13 21:04:19 -0500

    Solve AOC 2021 Day 12 part 2

--- src/main/kotlin/dev/tasso/adventofcode/_2021/day12/Day12.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2021/day12/Day12.kt
@@ -5,12 +5,14 @@ import dev.tasso.adventofcode.Solution
 class Day12 : Solution<Int> {
     override fun part1(input: List<String>): Int {
 
-        return getPaths(parseCaveGraph(input)).size
+        return getPathsSmallCavesOnce(parseCaveGraph(input)).size
 
     }
 
     override fun part2(input: List<String>): Int {
-        TODO("Not yet implemented")
+
+        return getPathsSmallCavesTwice(parseCaveGraph(input)).size
+
     }
 }
 
@@ -31,7 +33,9 @@ fun parseCaveGraph(input: List<String>) : Map<String, List<String>> {
 
 }
 
-fun getPaths(caveMap : Map<String, List<String>>, currPath : List<String> = mutableListOf(), startNode : String = "start") : List<List<String>>{
+fun getPathsSmallCavesOnce(caveMap : Map<String, List<String>>,
+                           currPath : List<String> = mutableListOf(),
+                           startNode : String = "start") : List<List<String>>{
 
     val paths = mutableListOf<List<String>>()
 
@@ -49,17 +53,57 @@ fun getPaths(caveMap : Map<String, List<String>>, currPath : List<String> = muta
 
             if (!(node.isLowerCase() && currPath.contains(node))) {
 
-                paths.addAll(getPaths(caveMap, newPath, node))
+                paths.addAll(getPathsSmallCavesOnce(caveMap, newPath, node))
 
             }
         }
 
     }
 
+    return paths
 
+}
+
+fun getPathsSmallCavesTwice(caveMap : Map<String, List<String>>,
+                            currPath : List<String> = mutableListOf(),
+                            startNode : String = "start") : List<List<String>>{
+
+    val paths = mutableListOf<List<String>>()
+
+    val newPath = currPath.toMutableList()
+    newPath.add(startNode)
+
+    if(startNode == "end" ) {
+
+        paths.add(newPath)
+
+    }
+    else {
+
+        caveMap[startNode]!!.forEach { node ->
+
+            if(node.isUpperCase()) {
+                paths.addAll(getPathsSmallCavesTwice(caveMap, newPath, node))
+            }else {
+                if(node != "start") {
+                    if(!newPath.hasTwoSmallCaveVisits()) {
+                        paths.addAll(getPathsSmallCavesTwice(caveMap, newPath, node))
+                    } else if(newPath.hasTwoSmallCaveVisits() && !currPath.contains(node)) {
+                        paths.addAll(getPathsSmallCavesTwice(caveMap, newPath, node))
+
+                    }
+                }
+            }
+
+        }
+    }
 
     return paths
 
 }
 
 fun String.isLowerCase() = this.all { it.isLowerCase() }
+
+fun String.isUpperCase() = !this.isLowerCase()
+
+fun List<String>.hasTwoSmallCaveVisits() = this.filter { it.isLowerCase() }.groupingBy { it }.eachCount().filter { it.value >= 2 }.isNotEmpty()
--- src/test/kotlin/dev/tasso/adventofcode/_2021/day12/Day12Test.kt
+++ src/test/kotlin/dev/tasso/adventofcode/_2021/day12/Day12Test.kt
@@ -2,11 +2,35 @@ package dev.tasso.adventofcode._2021.day12
 
 import dev.tasso.adventofcode.solutionTest
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 
 internal class Day12Test : BehaviorSpec({
 
-    include(solutionTest(Day12()::part1, 10, "2021/Day12_test_pt1_01.txt"))
-    include(solutionTest(Day12()::part1, 19, "2021/Day12_test_pt1_02.txt"))
-    include(solutionTest(Day12()::part1, 226, "2021/Day12_test_pt1_03.txt"))
+    include(solutionTest(Day12()::part1, 10, "2021/Day12_test_01.txt"))
+    include(solutionTest(Day12()::part1, 19, "2021/Day12_test_02.txt"))
+    include(solutionTest(Day12()::part1, 226, "2021/Day12_test_03.txt"))
+    include(solutionTest(Day12()::part2, 36, "2021/Day12_test_01.txt"))
+    include(solutionTest(Day12()::part2, 103, "2021/Day12_test_02.txt"))
+    include(solutionTest(Day12()::part2, 3509, "2021/Day12_test_03.txt"))
+
+    Given("a path with two small cave visits") {
+        val path = listOf("start", "a", "B", "c", "a")
+        When("checking if two small caves have been visited") {
+            Then("return true") {
+                path.hasTwoSmallCaveVisits() shouldBe true
+            }
+
+        }
+    }
+
+    Given("a path with only single small cave visits") {
+        val path = listOf("start", "a", "B", "c", "d")
+        When("checking if two small caves have been visited") {
+            Then("return false") {
+                path.hasTwoSmallCaveVisits() shouldBe false
+            }
+
+        }
+    }
 
 })
