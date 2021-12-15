43fd66a32ddcdf44967d10fb8d2ce9f78ad32926
Andrew A. Tasso <andrew@tasso.dev>
2021-12-15 13:00:24 -0500

    Solve AOC 2021 Day 14 part 2

--- src/main/kotlin/dev/tasso/adventofcode/_2021/day14/Day14.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2021/day14/Day14.kt
@@ -1,55 +1,75 @@
 package dev.tasso.adventofcode._2021.day14
 
 import dev.tasso.adventofcode.Solution
+import java.math.BigInteger
 
-class Day14 : Solution<Int> {
+class Day14 : Solution<BigInteger> {
 
-    override fun part1(input: List<String>): Int {
+    override fun part1(input: List<String>): BigInteger {
 
-        var polymerPairMap = input[0].windowed(2)
-                                     .groupingBy { it }
-                                     .eachCount()
-                                     .toMutableMap()
+        return runPolymerization(input, 10)
 
-        val pairInsertionMap = input.subList(2, input.size)
-                                    .map { it.split(" -> ") }
-                                    .associate { Pair(it[0], it[1]) }
+    }
 
-        for(i in 1..10) {
+    override fun part2(input: List<String>): BigInteger {
 
-            val tempMap = mutableMapOf<String, Int>()
+        return runPolymerization(input, 40)
 
-            polymerPairMap.forEach { (originalPair, pairTotal) ->
+    }
 
-                val insertPolymer = pairInsertionMap[originalPair]
-                val firstNewPair = originalPair[0] + insertPolymer!!
-                val secondNewPair = insertPolymer!! + originalPair[1]
 
-                tempMap[firstNewPair] = tempMap.getOrElse(firstNewPair){0} + pairTotal
-                tempMap[secondNewPair] = tempMap.getOrElse(secondNewPair){0} + pairTotal
+}
 
-            }
+fun runPolymerization(input: List<String>, numIterations: Int) : BigInteger {
 
-            polymerPairMap = tempMap.toMutableMap()
+    var polymerPairMap = input[0].windowed(2)
+                                 .groupingBy { it }
+                                 .eachCount()
+                                 .map{Pair(it.key, it.value.toBigInteger())}
+                                 .toMap()
+                                 .toMutableMap()
+
+    val pairInsertionMap = input.subList(2, input.size)
+                                .map { it.split(" -> ") }
+                                .associate { Pair(it[0], it[1]) }
+
+    for(i in 1..numIterations) {
+
+        val tempMap = mutableMapOf<String, BigInteger>()
+
+        polymerPairMap.forEach { (originalPair, pairTotal) ->
+
+            val insertPolymer = pairInsertionMap[originalPair]
+            val firstNewPair = originalPair[0] + insertPolymer!!
+            val secondNewPair = insertPolymer + originalPair[1]
+
+            tempMap[firstNewPair] = tempMap.getOrElse(firstNewPair){BigInteger.valueOf(0)} + pairTotal
+            tempMap[secondNewPair] = tempMap.getOrElse(secondNewPair){BigInteger.valueOf(0)} + pairTotal
 
         }
 
-        val polymerToCountMap =
-            listOf('B', 'C', 'H', 'N')
-                .associateWith { polymer -> polymerPairMap.filter { it.key.startsWith(polymer) }.map{it.value}.sum() }
-                .toMutableMap()
-
-        //After the mapping, we can't easily tell what the last character is. Since we're expanding pairs, the last
-        //character of the input is the last character of our resultant String
-        //be tacked onto the totals later
-        polymerToCountMap[input[0].last()] = polymerToCountMap[input[0].last()]!! + 1
-
-        return polymerToCountMap.maxOf { it.value } - polymerToCountMap.minOf { it.value }
+        polymerPairMap = tempMap.toMutableMap()
 
     }
 
-    override fun part2(input: List<String>): Int {
-        TODO("Not yet implemented")
-    }
+    val polymerToCountMap =  pairInsertionMap.map { it.value }
+            .associateWith { polymer -> polymerPairMap.filter { it.key.startsWith(polymer) }.map{it.value}.sum() }
+            .toMutableMap()
 
+    //After the mapping, we can't easily tell what the last character is. Since we're expanding pairs, the last
+    //character of the input is the last character of our resultant String
+    //be tacked onto the totals later
+    polymerToCountMap[input[0].last().toString()] =
+        polymerToCountMap[input[0].last().toString()]!! + BigInteger.valueOf(1)
+
+    return polymerToCountMap.maxOf { it.value } - polymerToCountMap.minOf { it.value }
+
+}
+
+fun Iterable<BigInteger>.sum(): BigInteger {
+    var sum: BigInteger = BigInteger.ZERO
+    for (element in this) {
+        sum += element
+    }
+    return sum
 }
--- src/test/kotlin/dev/tasso/adventofcode/_2021/day14/Day14Test.kt
+++ src/test/kotlin/dev/tasso/adventofcode/_2021/day14/Day14Test.kt
@@ -2,9 +2,12 @@ package dev.tasso.adventofcode._2021.day14
 
 import dev.tasso.adventofcode.solutionTest
 import io.kotest.core.spec.style.BehaviorSpec
+import java.math.BigInteger
 
 internal class Day14Test : BehaviorSpec({
 
-    include(solutionTest(Day14()::part1, 1588, "2021/day14_test.txt"))
+    include(solutionTest(Day14()::part1, BigInteger.valueOf(1588), "2021/day14_test.txt"))
+    include(solutionTest(Day14()::part2, BigInteger.valueOf(2188189693529), "2021/day14_test.txt"))
+
 
 })
