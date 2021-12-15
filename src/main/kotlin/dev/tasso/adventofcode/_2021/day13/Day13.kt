965a3b2c0e2076187c6cb498a1136ba609cbb301
Andrew A. Tasso <andrew@tasso.dev>
2021-12-14 23:52:01 -0500

    Still no quite working, but a step in the right direction

--- src/main/kotlin/dev/tasso/adventofcode/_2021/day13/Day13.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2021/day13/Day13.kt
@@ -9,7 +9,7 @@ class Day13 : Solution<Int> {
                                   .map{ it.split(",").map { it.toInt() }}
 
         val foldInstructions = input.filter { it.startsWith("fold") }
-                                    .map { it.substring(11, 14) }
+                                    .map { it.substring(11, it.length) }
                                     .map { it.split("=") }
                                     .map {Pair(it[0], it[1].toInt())}
 
@@ -18,20 +18,27 @@ class Day13 : Solution<Int> {
 
         val dotMap = (0 until width).map { (0 until height).map { 0 }.toIntArray() }.toList().toTypedArray()
 
-        dotMap[3][0] = 1
 
         for(coord in dotCoordinates) {
             dotMap[coord.first()][coord.last()] = 1
         }
 
-        val foldedMap = foldMap(dotMap, foldInstructions[0])
-//        val foldedMap = foldMap(dotMap, foldInstructions[1])
+        dotMap.forEach { it.forEach { print("$it ,") }
+            println()}
 
+        println()
 
-//        foldedMap.forEach { it.forEach { print("$it ,") }
-//            println()}
+        val folded = if(foldInstructions[0].first == "y")
+            foldUp(dotMap, foldInstructions[0].second)
+        else
+            foldLeft(dotMap, foldInstructions[0].second)
 
-        return foldedMap.sumOf { it.sum() }
+        folded.forEach { it.forEach { print("$it ,") }
+            println()}
+//
+//        println()
+
+        return folded.sumOf { it.sum() }
 
     }
 
@@ -40,23 +47,67 @@ class Day13 : Solution<Int> {
     }
 }
 
-fun foldMap(origMap: Array<IntArray>, fold: Pair<String, Int>) : Array<IntArray> {
+fun foldUp(origMap: Array<IntArray>, mirrorPoint: Int) : Array<IntArray> {
 
     val origWidth = origMap.size
     val origHeight = origMap[0].size
-    val foldedHeight = if(fold.first == "y") origHeight / 2 else origHeight
-    val foldedWidth = if(fold.first == "x") origWidth / 2 else origWidth
+    val foldedHeight = origHeight - mirrorPoint - 1
 
-    val foldedMap = (0 until foldedWidth).map {  (0 until foldedHeight).map { 0 }.toIntArray() }.toList().toTypedArray()
+    val foldedMap = (0 until origWidth).map {  (0 until foldedHeight).map { 0 }.toIntArray() }.toList().toTypedArray()
 
-    for(x in 0 until foldedWidth) {
+    for(x in 0 until origWidth) {
         for(y in 0 until foldedHeight) {
-            val upper = origMap[x][y]
-            val lower = origMap[x][origHeight - y - 1]
+
+            val upper = origMap[x][mirrorPoint - y - 1]
+            val lower = origMap[x][mirrorPoint + y + 1]
             val value = upper or lower
-            foldedMap[x][y] = value
+            foldedMap[x][mirrorPoint - y - 1] = value
         }
     }
 
     return foldedMap
 }
+
+fun foldLeft(origMap: Array<IntArray>, mirrorPoint : Int) : Array<IntArray> {
+
+    val origWidth = origMap.size
+    val origHeight = origMap[0].size
+    val foldedWidth = origWidth - mirrorPoint - 1
+
+    val foldedMap = (0 until mirrorPoint).map {  (0 until origHeight).map { 0 }.toIntArray() }.toList().toTypedArray()
+    for(x in 0 until mirrorPoint)
+        for(y in 0 until origHeight)
+            foldedMap[x][y] = origMap[x][y]
+
+    for(x in 0 until foldedWidth) {
+        for(y in 0 until origHeight) {
+            val left = origMap[mirrorPoint - x - 1][y]
+            val right = origMap[mirrorPoint + x + 1][y]
+            val value = left or right
+            foldedMap[mirrorPoint - x - 1][y] = value
+        }
+    }
+
+    return foldedMap
+}
+
+//fun foldLeftOld(origMap: Array<IntArray>, fold: Pair<String, Int>) : Array<IntArray> {
+//
+//    val origWidth = origMap.size
+//    val origHeight = origMap[0].size
+//    val foldedHeight = if(fold.first == "y") origHeight / 2 else origHeight
+//    val foldedWidth = if(fold.first == "x") origWidth / 2 else origWidth
+//
+//    val foldedMap = (0 until foldedWidth).map {  (0 until foldedHeight).map { 0 }.toIntArray() }.toList().toTypedArray()
+//
+//    for(x in 0 until foldedWidth) {
+//        for(y in 0 until foldedHeight) {
+//            val left = origMap[x][y]
+//            val right = origMap[origWidth - x - 1][y]
+//            val value = left or right
+//            foldedMap[x][y] = value
+//        }
+//    }
+//
+//    return foldedMap
+//}
--- src/test/kotlin/dev/tasso/adventofcode/_2021/day13/Day13Test.kt
+++ src/test/kotlin/dev/tasso/adventofcode/_2021/day13/Day13Test.kt
@@ -5,6 +5,6 @@ import io.kotest.core.spec.style.BehaviorSpec
 
 internal class Day13Test : BehaviorSpec({
 
-    include(solutionTest(Day13()::part1, 17, "2021/day13_test.txt"))
+    include(solutionTest(Day13()::part1, 17, "2021/day13_test_02.txt"))
 
 })
