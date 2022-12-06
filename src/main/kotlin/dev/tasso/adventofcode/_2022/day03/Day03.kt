25cb1a5184ba99a07341bb51d1a8b7e7416d371f
Andrew A. Tasso <andrew@tasso.dev>
2022-12-05 22:13:51 -0500

    Clean up Day 03 implementation a bit

--- src/main/kotlin/dev/tasso/adventofcode/_2022/day03/Day03.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2022/day03/Day03.kt
@@ -7,13 +7,13 @@ class Day03 : Solution<Int> {
     override fun part1(input: List<String>): Int =
         input.map { Pair(it.toCharArray(0, it.length / 2), it.toCharArray(it.length / 2)) }
              .map { it.first.intersect(it.second.toSet())}
-             .map{ it.toList()[0] }
+             .map{ it.single() }
              .sumOf { if (it.isUpperCase()) it.code - 38 else it.code - 96 }
 
     override fun part2(input: List<String>): Int =
         input.chunked(3)
              .map { it[0].toSet().intersect(it[1].toSet()).intersect(it[2].toSet()) }
-             .map { it.toList()[0] }
+             .map { it.single() }
              .sumOf { if (it.isUpperCase()) it.code - 38 else it.code - 96 }
 
 }
