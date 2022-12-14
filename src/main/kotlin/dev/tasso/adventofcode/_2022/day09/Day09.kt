a2526f90eadcad8a7483c5c8f76575153fdfc46e
Andrew A. Tasso <andrew@tasso.dev>
2022-12-13 22:05:18 -0500

    Solve AOC 2022 Day 9 Pt. 2

--- src/main/kotlin/dev/tasso/adventofcode/_2022/day09/Day09.kt
+++ src/main/kotlin/dev/tasso/adventofcode/_2022/day09/Day09.kt
@@ -10,51 +10,61 @@ class Day09 : Solution<Int> {
              //Break each larger step of multiple moves in a specific direction into moves of only a single space each
              .flatMap { splitLine -> List(splitLine[1].toInt()) { splitLine[0].first() }}
              // Provide a list to hold the positions the rope will travel, starting with both the head and tail at 0,0
-             .fold( listOf(Rope()) ) { positions : List<Rope>, moveDirection : Char ->
+             .fold( listOf(Rope(List(2){ Coords() })) ) { positions : List<Rope>, moveDirection : Char ->
                  positions.plus(positions.last().movedTo(moveDirection))
-             }.distinctBy { position -> position.tailCoords }
+             }.distinctBy { position -> position.knotCoords.last()}
             .count()
-
-    override fun part2(input: List<String>): Int {
-
-        return 0
-    }
+    override fun part2(input: List<String>): Int =
+        input.map { inputLine -> inputLine.split(" ") }
+             //Break each larger step of multiple moves in a specific direction into moves of only a single space each
+             .flatMap { splitLine -> List(splitLine[1].toInt()) { splitLine[0].first() }}
+             // Provide a list to hold the positions the rope will travel, starting with both the head and tail at 0,0
+             .fold( listOf(Rope(List(10){ Coords() })) ) { positions : List<Rope>, moveDirection : Char ->
+                 positions.plus(positions.last().movedTo(moveDirection))
+             }.distinctBy { position -> position.knotCoords.last()}
+             .count()
 
     private data class Coords(val x: Int = 0, val y: Int = 0)
 
-    private data class Rope(val headCoords: Coords = Coords(0,0), val tailCoords: Coords = Coords(0,0))
+    private data class Rope(val knotCoords: List<Coords>)
 
     private fun Rope.movedTo(direction : Char) : Rope {
 
+        val headCoords = this.knotCoords.first()
+
         fun getNewHeadCoords(moveDirection: Char) : Coords =
             when(moveDirection) {
-                'U' -> Coords(this.headCoords.x, this.headCoords.y + 1)
-                'D' -> Coords(this.headCoords.x, this.headCoords.y - 1)
-                'R' -> Coords(this.headCoords.x + 1, this.headCoords.y)
-                'L' -> Coords(this.headCoords.x - 1, this.headCoords.y)
+                'U' -> Coords(headCoords.x, headCoords.y + 1)
+                'D' -> Coords(headCoords.x, headCoords.y - 1)
+                'R' -> Coords(headCoords.x + 1, headCoords.y)
+                'L' -> Coords(headCoords.x - 1, headCoords.y)
                 else -> throw IllegalArgumentException("Unexpected direction encountered ($moveDirection)")
             }
 
-        fun getNewTailCoords(newHeadCoords : Coords) : Coords =
-            if(abs(this.tailCoords.x - newHeadCoords.x) >= 2) {
-                if(this.tailCoords.y == newHeadCoords.y) {
-                    Coords((newHeadCoords.x + this.tailCoords.x) / 2, this.tailCoords.y)
+        fun getNewKnotCoords(nextKnotCoords : Coords, prevKnotCoords : Coords) : Coords =
+            if(abs(prevKnotCoords.x - nextKnotCoords.x) >= 2 && abs(prevKnotCoords.y - nextKnotCoords.y) >= 2) {
+                Coords((nextKnotCoords.x + prevKnotCoords.x) / 2, (nextKnotCoords.y + prevKnotCoords.y) / 2)
+            } else if(abs(prevKnotCoords.x - nextKnotCoords.x) >= 2) {
+                if(prevKnotCoords.y == nextKnotCoords.y) {
+                    Coords((nextKnotCoords.x + prevKnotCoords.x) / 2, prevKnotCoords.y)
                 } else { //if diagonal, bring the tail in line with the new position of the head
-                    Coords((newHeadCoords.x + this.tailCoords.x) / 2, newHeadCoords.y)
+                    Coords((nextKnotCoords.x + prevKnotCoords.x) / 2, nextKnotCoords.y)
                 }
-            } else if(abs(this.tailCoords.y - newHeadCoords.y) >= 2) {
-                if(this.tailCoords.y == newHeadCoords.y) {
-                    Coords(this.tailCoords.x, (newHeadCoords.y + this.tailCoords.y) / 2)
+            } else if(abs(prevKnotCoords.y - nextKnotCoords.y) >= 2) {
+                if(prevKnotCoords.y == nextKnotCoords.y) {
+                    Coords(prevKnotCoords.x, (nextKnotCoords.y + prevKnotCoords.y) / 2)
                 } else { //if diagonal, bring the tail in line with the new position of the head
-                    Coords(newHeadCoords.x, (newHeadCoords.y + this.tailCoords.y) / 2)
+                    Coords(nextKnotCoords.x, (nextKnotCoords.y + prevKnotCoords.y) / 2)
                 }
             } else {
-                this.tailCoords
+                prevKnotCoords
             }
 
-        return getNewHeadCoords(direction).let { newHeadCoords ->
-            Rope(newHeadCoords , getNewTailCoords(newHeadCoords))
-        }
+        return this.knotCoords.drop(1) //Drop old head knot since the new list starts with the new head knot
+                              //Start the new rope with the new head coords and drag the remaining knots along for the ride
+                              .fold(listOf(getNewHeadCoords(direction))) { newKnotCoords, currOldKnotCoord ->
+                                  newKnotCoords.plus(getNewKnotCoords(newKnotCoords.last(), currOldKnotCoord))
+                              }.let { Rope(it) }
 
     }
 }
--- src/test/kotlin/dev/tasso/adventofcode/_2022/day09/Day09Test.kt
+++ src/test/kotlin/dev/tasso/adventofcode/_2022/day09/Day09Test.kt
@@ -5,7 +5,10 @@ import io.kotest.core.spec.style.BehaviorSpec
 
 class Day09Test : BehaviorSpec({
 
-    include(solutionTest(Day09()::part1, 13, "2022/day09_test.txt"))
-//    include(solutionTest(Day09()::part2, 8, "2022/day09_test.txt"))
+    include(solutionTest(Day09()::part1, 13, "2022/day09_test_01.txt"))
+    include(solutionTest(Day09()::part1, 88, "2022/day09_test_02.txt"))
+
+    include(solutionTest(Day09()::part2, 1, "2022/day09_test_01.txt"))
+    include(solutionTest(Day09()::part2, 36, "2022/day09_test_02.txt"))
 
 })
